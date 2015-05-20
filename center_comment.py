7d55a303d46806850609d8a08e0b96fbdfdece31
Michael Hunter <michael.hunter@cooltrax.com>
2015-05-20 11:22:41 +1000

    Added readme; Fixed bug; Added additional key maps for OSX and Linux

--- center_comment.py
+++ center_comment.py
@@ -7,21 +7,24 @@ class CenterCommentCommand(sublime_plugin.TextCommand):
             for line in lines:
                 text = self.view.substr(line)
                 # // -------------------------- abc --------------------------
-                m = re.match(r'^(.*?)(//+)(\s?)\s*([-=*]*)\s*?(\s?)(.+?)([ -=*]*?)([ ]?)()$', text)
+                m = re.match(r'^(.*?)(//+)(\s?)\s*([-=*]*)\s*?(\s?)(\w.*?)?([ -=*]*?)([ ]?)()$', text)
 
                 # Try C style comment
                 # /* ------------------------- abc ------------------------- */
                 if not m:
-                    m = re.match(r'^(.*?)(/\*+)(\s?)\s*([-=*]*)\s*?(\s?)(.+?)([ -=*]*?)([ ]?)(\*/)$', text)
+                    m = re.match(r'^(.*?)(/\*+)(\s?)\s*([-=*]*)\s*?(\s?)(\w.*?)?([ -=*]*?)([ ]?)(\*/)$', text)
 
                 if m:
-                    # print(m.groups())
+                    print(m.groups())
+                    titleText = m.group(6)
+                    if not titleText:
+                        titleText = ''
                     titleSpace = m.group(5) # Space on either side of the title
                     if len(m.group(4)) > 0:
                         padChar = m.group(4)[0]
                     else:
-                        padChar = '-'
-                        titleSpace = ' '
+                        padChar = ' '
+                        titleSpace = ''
 
                     trailingCommentMark = m.group(9)
                     if trailingCommentMark != '':
@@ -29,16 +32,17 @@ class CenterCommentCommand(sublime_plugin.TextCommand):
                     else:
                         trailingSpace = ''
 
-                    lengthExclPad = len(m.group(1)) + len(m.group(2)) + len(m.group(3)) + len(titleSpace) * 2 + len(m.group(6)) + len(trailingSpace) + len(trailingCommentMark)
+                    lengthExclPad = len(m.group(1)) + len(m.group(2)) + len(m.group(3)) + len(titleSpace) * 2 + len(titleText) + len(trailingSpace) + len(trailingCommentMark)
                     wrapWidth = self.view.settings().get('wrap_width')
                     if wrapWidth == 0:
-                        wrapWidth = 79
+                        wrapWidth = 80
+                    wrapWidth -= 1
 
                     numToAdd = int((wrapWidth - lengthExclPad) / 2)
                     if numToAdd < 0:
                         numToAdd = 0
 
-                    newText = m.group(1) + m.group(2) + m.group(3) + (padChar * numToAdd) + titleSpace + m.group(6) + titleSpace + (padChar * numToAdd) + trailingSpace + trailingCommentMark
+                    newText = m.group(1) + m.group(2) + m.group(3) + (padChar * numToAdd) + titleSpace + titleText + titleSpace + (padChar * numToAdd) + trailingSpace + trailingCommentMark
 
                     # Remove trailing spaces, for the case that the padding char is itself a space
                     newText = re.sub(r'\s+$', '', newText)
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Sublime Center Comment
+
+A plugin for Sublime Text to center C/C++ style comments such this:
+
+    // ----------------------------- Section Here -----------------------------
+
+This also works for C-like languages such as C#, Java, and JavaScript.
+
+
+## Usage
+
+The default shortcut key is `ctrl+shift+c` or `super+shift+c`, and will center commented text on lines under the current cursor(s).
+
+Here are some examples, showing first the line on which the command was executed, and then the result:
+
+    // Padded with spaces
+    //                            Padded with spaces
+
+    // -Dashes
+    // ---------------------------------Dashes---------------------------------
+
+    // - Extra Space
+    // ----------------------------- Extra Space -----------------------------
+
+    //-No space
+    //--------------------------------No space--------------------------------
+
+    // = Equals
+    // ================================ Equals ================================
+
+    // * Asterisks
+    // ****************************** Asterisks ******************************
+
+    // -
+    // ------------------------------------------------------------------------
+
+    // = Multi -- Part
+    // ============================ Multi -- Part ============================
+
+    /* - C Style */
+    /* ------------------------------ C Style ------------------------------ */
+
+
+
